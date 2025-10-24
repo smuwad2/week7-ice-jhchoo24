9518e8ded3dfeadd25d0cfab381e875d65a1970f
jhchoo24 <jh.choo.2024@smu.edu.sg>
2025-10-24 03:52:47 +0000

    ex1,2,3

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -3,16 +3,34 @@
         // Add code in appropriate places
         data() {
             return {
+                newItem: "",
                 items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter']
             }
+        },
+        methods: {
+            addItem()
+            {
+                this.items.push(this.newItem)
+
+            }
+        }
             
         }
-    }
+    
 </script>
 
 <template>
 
     <h2>Shopping Cart</h2>
     <!-- TODO: Add Code Here -->
+     <ul>
+            <li v-for="(item, idx) in items">
+                {{ item }}
+            <button @click="items.splice(idx, 1)">Delete!</button>
+            </li>
+     </ul>
+
+     <input type="text" v-model="newItem"></input>
+     <button @click="addItem">Add!</button>
    
 </template>
--- src/components/Ex3.vue
+++ src/components/Ex3.vue
@@ -1,9 +1,43 @@
 <script>
-    export default { 
+import axios from 'axios';
+export default {
+    // add code here
+    data() {
+        return {
+            subject: "",
+            entry: "",
+            moods: ["Happy", "Sad", "Angry"],
+            selected_mood: "",
+            outputMsg: ""
+        }
+    },
 
-       // add code here
-
+    computed: {
+        baseUrl() {
+            if (window.location.hostname == 'localhost')
+                return 'http://localhost:3000'
+            else {
+                const codespace_host = window.location.hostname.replace('5173', '3000')
+                return https://${codespace_host};
+            }
+        }
+    },
+    methods: {
+        addPost() {
+            axios.get(${this.baseUrl}/addPost, {
+                params: {
+                    subject: this.subject,
+                    entry: this.entry,
+                    mood: this.selected_mood
+                }
+            }).then(response => {
+                this.outputMsg = response.data.message;
+            }).catch(error => {
+                console.log(error);
+            })
+        }
     }
+}
 </script>
 
 <template>
@@ -19,14 +53,16 @@
 
         Mood:
         <!-- TODO: Build a dropdown list here for selecting the mood -->
+        <select v-model="selected_mood">
+            <option v-for="mood in moods">{{ mood }}</option>
+        </select>
 
         <br>
 
         <br>
-        <button>Submit New Post</button>
+        <button @click="addPost">Submit New Post</button>
+        {{ outputMsg }}
+        <hr> Click <a><router-link to="/ViewPosts/">here</router-link></a> to return to Main Page
 
-        <hr> Click  <a><router-link to="/ViewPosts/">here</router-link></a>  to return to Main Page
-       
     </div>
 </template>
-
